b47d9d911508b2b5259cd07974c90b934e5c8a80
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-05-07 23:40:15 +1100

    v 0.2

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Alexander Kryuchkov on 07.05.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
